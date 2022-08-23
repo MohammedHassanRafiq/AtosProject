6d1d66b230d21680722aa425e257551659531975
MohammedHassanRafiq <110388368+MohammedHassanRafiq@users.noreply.github.com>
2022-08-23 12:58:36 +0100

    Delete Atos-Project directory

--- Atos-Project/src/main/java/com/qa/atos/DBconfig.java
+++
@@ -1,9 +0,0 @@
-package com.qa.atos;
-
-public class DBconfig {
- 
-	protected static final String URL = "jdbc:mysql://localhost:3306/royalmail";
-	protected static final String USER = "root";
-	protected static final String PASS = "root";
-
-}
